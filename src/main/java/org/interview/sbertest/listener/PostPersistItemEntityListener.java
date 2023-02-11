b7b405311c8df0dfdb3936e1977290dc01754532
Yuri Medvedev <megapalestiner@gmail.com>
2023-02-12 01:27:45 +0700

    added log4j
    changed PostPersistItemEntityListener logging logic

--- src/main/java/org/interview/sbertest/listener/PostPersistItemEntityListener.java
+++ src/main/java/org/interview/sbertest/listener/PostPersistItemEntityListener.java
@@ -1,11 +1,17 @@
 package org.interview.sbertest.listener;
 
-import javax.persistence.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.persistence.PostPersist;
 
 public class PostPersistItemEntityListener {
 
+    private static final Log LOG = LogFactory.getLog(PostPersistItemEntityListener.class);
+
     @PostPersist
     public void postPersist(Object o) {
-        System.out.println(o);
+        LOG.info("insert into ItemEntity (id, createdAt) values (?, ?)");
+        LOG.info(o);
     }
 }
